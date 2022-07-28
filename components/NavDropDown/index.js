bc330b1202386697b1978bc225c5ab48a50f3965
sansarj17 <93729019+sansarj17@users.noreply.github.com>
2022-07-28 23:56:37 +0530

    eslint errors resolved

--- components/NavDropDown/index.js
+++ components/NavDropDown/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import React from 'react'
 import { AiFillPhone, AiOutlineMail } from 'react-icons/ai'
 import { FaLocationArrow } from "react-icons/fa"
